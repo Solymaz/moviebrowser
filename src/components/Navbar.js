0e0afb63a1170c33841adf78776b8b765496759a
Solmaz <44635076+Solymaz@users.noreply.github.com>
2021-07-27 13:49:56 +0200

    added home and about link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="b">
+        <Link className="navbar-brand" to="/">
           Movie Browser
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -19,24 +21,24 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="b">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="b">
-                Link
-              </a>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
             </li>
             <li className="nav-item">
-              <a
+              <Link
                 className="nav-link disabled"
-                href="b"
+                to="b"
                 tabindex="-1"
                 aria-disabled="true"
               >
                 Disabled
-              </a>
+              </Link>
             </li>
           </ul>
           <form className="d-flex">
