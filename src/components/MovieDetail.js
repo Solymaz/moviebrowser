ace7b8d32f44852ed290ec2845832cab41838572
Solmaz <44635076+Solymaz@users.noreply.github.com>
2021-07-29 16:22:50 +0200

    show movie detail

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -6,6 +6,7 @@ function MovieDetail() {
   const { id } = useParams();
   const [movieDetails, setMovieDetails] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const posterPath = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`;
 
   useEffect(() => {
     fetch(
@@ -13,13 +14,44 @@ function MovieDetail() {
     )
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
         setMovieDetails(data);
         setIsLoading(false);
       });
   }, [id]);
+  //todo: deal with a missing img
   return (
     <>
-      <Hero text={!isLoading ? movieDetails.original_title : "... loading 🍿"} />
+      <Hero
+        text={!isLoading ? movieDetails.original_title : "... loading 🍿"}
+      />
+
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-md-3">
+            <img
+              src={posterPath}
+              alt="..."
+              className="img-fluid shadow rounded"
+            />
+          </div>
+          <div className="col-md-9">
+            <p className="lead">Overview: {movieDetails.overview}</p>
+            {movieDetails.genres && movieDetails.genres.length > 0 && (
+              <p>Genres: {movieDetails.genres[0].name}</p>
+            )}
+            {movieDetails.vote_average && (
+              <p>Ranking: {movieDetails.vote_average} / 10</p>
+            )}
+            {movieDetails.release_date && (
+              <p>Release date: {movieDetails.release_date} </p>
+            )}
+            {movieDetails.runtime && (
+              <p>Duration: {movieDetails.runtime} minuts</p>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
